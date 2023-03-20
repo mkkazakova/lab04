8994aa664999a89f8737821bbdd8351248ff66a0
Masha <mkkazakova@yandex.ru>
2023-03-20 18:07:12 +0300

    hello world

---
+++ hello_world_application/_build/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/masha/lab-03/hello_world_application/hello_world.cpp" "CMakeFiles/example.dir/hello_world.cpp.o" "gcc" "CMakeFiles/example.dir/hello_world.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/masha/lab-03/hello_world_application/_build/formatter_ex/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/home/masha/lab-03/hello_world_application/_build/formatter_ex/formatter/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hello_world_application/_build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/hello_world.cpp.o"
+  "CMakeFiles/example.dir/hello_world.cpp.o.d"
+  "example"
+  "example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
